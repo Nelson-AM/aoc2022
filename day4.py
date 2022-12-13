e8237f6a8d8409164987b7f9db83f93820b9338d
Nelson <hello@nelson.science>
2022-12-13 07:30:11 +0100

    day 4 puzzle 2

--- day4.py
+++ day4.py
@@ -20,17 +20,35 @@ def convert_file_list_to_sets(file_list):
 def puzzle_one(file_list):
     list_with_sets = convert_file_list_to_sets(file_list)
 
-    contained_pairs = 0
+    return find_pairs_with_subsets(list_with_sets)
+
+
+def find_pairs_with_subsets(list_with_sets):
+    subset_pairs = 0
     for pair in list_with_sets:
-        print(f'pair_one: {pair[0]}, pair_two: {pair[1]}')
         if pair[0].issubset(pair[1]) or pair[1].issubset(pair[0]):
-            print('subset detected!')
-            contained_pairs += 1
-    return contained_pairs
+            subset_pairs += 1
+    return subset_pairs
+
+
+def find_pairs_with_intersection(list_with_sets):
+    intersection_pairs = 0
+
+    for pair in list_with_sets:
+        if len(pair[0] & pair[1]) != 0:
+            intersection_pairs += 1
+    return intersection_pairs
+
+
+def puzzle_two(file_list):
+    list_with_sets = convert_file_list_to_sets(file_list)
+    return find_pairs_with_intersection(list_with_sets)
 
 
 if __name__ == '__main__':
     file_list = load_file_as_list('input/input_day_4')
     answer_one = puzzle_one(file_list)
+    answer_two = puzzle_two(file_list)
 
-    print_output(answer_one=answer_one)
+    print_output(answer_one=answer_one,
+                 answer_two=answer_two)
